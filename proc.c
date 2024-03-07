574f33fdb3a414b2821412faf70cf4b20856b7f6
sohamKumthekar <kumthekarss21.comp@coep.ac.in>
2024-03-08 00:55:12 +0530

    nice and ps system call corrected

--- proc.c
+++ proc.c
@@ -559,13 +559,13 @@ int cps(){
 	
 	acquire(&ptable.lock);
 	cprintf("name \t pid \t state \t priority \n");
-	for(p = ptabe.proc; p < &ptable.proc[NPROC]; p++){
+	for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
 		if(p->state == SLEEPING)
 		  cprintf("%s \t %d \t SLEEPING \t %d \n", p->name, p->pid, p->priority);
 		else if(p->state == RUNNING)
-		  cprintf("%s \t %d \t RUNNING \t %d \n", p->name, p->pid, p->priority)
+		  cprintf("%s \t %d \t RUNNING \t %d \n", p->name, p->pid, p->priority);
 		else if(p->state == RUNNABLE)
-		  cprintf("%s \t %d \t RUNNABLE \t %d \n", p->name, p->pid, p->priority)
+		  cprintf("%s \t %d \t RUNNABLE \t %d \n", p->name, p->pid, p->priority);
 	}
 	release(&ptable.lock);
 	return 24;
